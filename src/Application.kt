05cc05e84cfe409b0e7af281566d09b32c78d168
Honwaka Developer <honwaka.developer@gmail.com>
2019-09-27 00:17:22 +0900

    HTMLファイルを読み込んで Hello World を表示

--- src/Application.kt
+++ src/Application.kt
@@ -1,11 +1,21 @@
 package div.honwakalab.hello
 
 import io.ktor.application.*
+import io.ktor.response.respondFile
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import java.io.File
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    routing {
+        get("/") {
+            call.respondFile(File("./resources/hello.html"))
+        }
+    }
 }
 
