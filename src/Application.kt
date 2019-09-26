f342d848e5d680d00bcda41a525b9c40512f5dc6
Honwaka Developer <honwaka.developer@gmail.com>
2019-09-27 01:29:26 +0900

    static メソッドを使用して HTML と CSS で Hello World を表示

--- src/Application.kt
+++ src/Application.kt
@@ -1,11 +1,22 @@
 package div.honwakalab.hello
 
 import io.ktor.application.*
+import io.ktor.http.content.default
+import io.ktor.http.content.file
+import io.ktor.http.content.static
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    routing {
+        static("/") {
+            file("hello.css", "./resources/hello.css")
+            default("resources/hello.html")
+        }
+    }
 }
 
