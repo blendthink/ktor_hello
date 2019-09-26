cbe943fb8404c7669556f9bcdf7f2c6cc0075199
Honwaka Developer <honwaka.developer@gmail.com>
2019-09-27 01:09:23 +0900

    resource というメソッドを使用して HTML ファイルで Hello World を表示

--- src/Application.kt
+++ src/Application.kt
@@ -1,11 +1,18 @@
 package div.honwakalab.hello
 
 import io.ktor.application.*
+import io.ktor.http.content.resource
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    routing {
+
+        resource("/", "hello.html")
+    }
 }
 
